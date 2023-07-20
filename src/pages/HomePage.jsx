f8cf6bf47fc8463e63417d0f0874288f80afeeaf
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-07-20 22:24:45 +0800

    add: categories view

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from 'react'
 import FeaturedProducts from '../components/home/FeaturedProducts'
+import Categories from '../components/home/Categories'
 class AppRoute extends Component {
      render() {
           return (
                <Fragment>
                     <FeaturedProducts />
+                    <Categories />
                </Fragment>
           )
      }
