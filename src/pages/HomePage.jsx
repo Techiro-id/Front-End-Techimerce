216330ad1606ab103c2f28c89bb024c72c96ef73
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-07-21 00:16:35 +0800

    add: responsitivity css and home top mobile component setup

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,13 +6,22 @@ import NewArrival from '../components/home/NewArrival'
 import HomeTop from '../components/home/HomeTop'
 import NavMenuDesktop from '../components/common/NavMenuDesktop'
 import NavMenuMobile from '../components/common/NavMenuMobile'
+import HomeTopMobile from '../components/home/HomeTopMobile'
 
 class AppRoute extends Component {
      render() {
           return (
             <Fragment> 
-                <NavMenuMobile />       
-                <HomeTop />  
+                <div className="Desktop">
+                    <NavMenuDesktop />
+                    <HomeTop />
+                </div>
+
+                <div className="Mobile">
+                    <NavMenuMobile />  
+                    <HomeTopMobile />
+                </div>
+                
                 <FeaturedProducts />
                 <NewArrival />
                 <Categories />
