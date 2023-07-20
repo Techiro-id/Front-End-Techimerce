f6d33ad6a4c864c958f0eb5ec3d59f8eab563ed1
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-07-20 22:22:14 +0800

    add: featured products initial listing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,10 @@
 import React, { Component, Fragment } from 'react'
-import { Router, Route, Switch } from "react-router";
-import HomePage from '../pages/HomePage';
-
+import FeaturedProducts from '../components/home/FeaturedProducts'
 class AppRoute extends Component {
      render() {
           return (
                <Fragment>
-                    <Switch>
-                         <Route exact to="/" component={HomePage} />
-                    </Switch>
-
+                    <FeaturedProducts />
                </Fragment>
           )
      }
